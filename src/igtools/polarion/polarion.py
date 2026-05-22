e00ee35cf5ffe2411d6ba821660c7bbd02f6ed37
Alexander Essenwanger <alexander.essenwanger@gematik.de>
2026-05-22 10:07:51 +0200

    sort polarion export

--- src/igtools/polarion/polarion.py
+++ src/igtools/polarion/polarion.py
@@ -10,7 +10,7 @@ from ..errors import FilePathNotExists, ExportFormatUnknown, BaseException
 from ..specifications import ReleaseManager
 
 # funkt. Eignung: Test Produkt/FA
-DEFAULT_TESTPROCEDURE = "testProcedurePT03" 
+DEFAULT_TESTPROCEDURE = "testProcedurePT03"
 
 
 class PolarionExportError(BaseException):
@@ -31,14 +31,18 @@ class PolarionExportConformanceError(BaseException):
 
 @lru_cache(maxsize=1)
 def load_polarion_mappings():
-    with resources.files("igtools").joinpath("mappings/polarion.yaml").open("r", encoding="utf-8") as f:
+    with resources.files("igtools").joinpath("mappings/polarion.yaml").open(
+        "r", encoding="utf-8"
+    ) as f:
         mappings = yaml.safe_load(f)
     return mappings.get("actor_to_product", {}), mappings.get("testproc_to_id", {})
 
 
 @lru_cache(maxsize=1)
 def load_test_procesure_default_mappings():
-    with resources.files("igtools").joinpath("mappings/polarion.yaml").open("r", encoding="utf-8") as f:
+    with resources.files("igtools").joinpath("mappings/polarion.yaml").open(
+        "r", encoding="utf-8"
+    ) as f:
         mappings = yaml.safe_load(f)
     return mappings.get("default_testproc", {})
 
@@ -56,12 +60,16 @@ def convert_polarion_date_export(value):
                 try:
                     dt = datetime.fromisoformat(f"{value[:-1]}+00:00")
                 except ValueError as exc:
-                    raise PolarionExportDateError(f"Unsupported string format for date: {value}") from exc
+                    raise PolarionExportDateError(
+                        f"Unsupported string format for date: {value}"
+                    ) from exc
             else:
                 try:
                     dt = datetime.strptime(value, "%Y-%m-%d")
                 except ValueError as exc:
-                    raise PolarionExportDateError(f"Unsupported string format for date: {value}") from exc
+                    raise PolarionExportDateError(
+                        f"Unsupported string format for date: {value}"
+                    ) from exc
     else:
         raise PolarionExportDateError(f"Unsupported type for date: {type(value)}")
 
@@ -74,13 +82,7 @@ def convert_polarion_date_export(value):
 
 class PolarionExporter:
     EXPORT_BASE_FILENAME = "polarion-requirements"
-    ALLOWED_CONFORMANCE = [
-        "SHALL",
-        "SHALL NOT",
-        "MAY",
-        "SHOULD",
-        "SHOULD NOT"
-    ]
+    ALLOWED_CONFORMANCE = ["SHALL", "SHALL NOT", "MAY", "SHOULD", "SHOULD NOT"]
 
     def __init__(self, config, ig_config, version=None, default_test_procedure=None):
         self.config = config
@@ -96,10 +98,13 @@ class PolarionExporter:
         if output_ext:
             filepath = output
         else:
-            base = f"{cls.EXPORT_BASE_FILENAME}-{version}" if version and version != "current" else cls.EXPORT_BASE_FILENAME
+            base = (
+                f"{cls.EXPORT_BASE_FILENAME}-{version}"
+                if version and version != "current"
+                else cls.EXPORT_BASE_FILENAME
+            )
             filepath = os.path.join(output, f"{base}{extension}")
         return filepath
-
 
     def get_default_test_proc(self, actor):
         DEFAULT_TEST_PROCEDURE_MAPPING = load_test_procesure_default_mappings()
@@ -107,8 +112,7 @@ class PolarionExporter:
         procedure = None
         if str(actor) in DEFAULT_TEST_PROCEDURE_MAPPING.keys():
             procedure = TESTPROC_MAPPING.get(
-                DEFAULT_TEST_PROCEDURE_MAPPING.get(str(actor)), 
-                None
+                DEFAULT_TEST_PROCEDURE_MAPPING.get(str(actor)), None
             )
         if procedure is None:
             procedure = TESTPROC_MAPPING.get(
@@ -116,12 +120,13 @@ class PolarionExporter:
             )
         return procedure
 
-
     def get_test_procedure(self, key, requirement):
         ACTOR_MAPPING, TESTPROC_MAPPING = load_polarion_mappings()
         procedure = TESTPROC_MAPPING.get(key, None)
         if procedure is None:
-            raise PolarionExportMappingError(f"❌ No test procedure mapping found for '{key}'. Source: {requirement.source}; requirement key: {requirement.key}.")
+            raise PolarionExportMappingError(
+                f"❌ No test procedure mapping found for '{key}'. Source: {requirement.source}; requirement key: {requirement.key}."
+            )
         return procedure
 
     def map_product_types(self, requirement):
@@ -132,13 +137,17 @@ class PolarionExporter:
             for actor, test_procedure in requirement.test_procedures.items():
                 product = ACTOR_MAPPING.get(actor, None)
                 if product is None:
-                    _errors.append(f"❌ No product type mapping found for actor '{actor}'. Source: {requirement.source}; requirement key: {requirement.key}.")
+                    _errors.append(
+                        f"❌ No product type mapping found for actor '{actor}'. Source: {requirement.source}; requirement key: {requirement.key}."
+                    )
                 product_type = {}
                 product_type["product_type"] = product
                 product_type["test_procedure"] = []
                 for tp in test_procedure:
                     try:
-                        procedure = self.get_test_procedure(key=tp, requirement=requirement)
+                        procedure = self.get_test_procedure(
+                            key=tp, requirement=requirement
+                        )
                         product_type["test_procedure"].append(procedure)
                     except PolarionExportMappingError as pe:
                         _errors.append(str(pe))
@@ -149,7 +158,9 @@ class PolarionExporter:
 
                 product_types.append(product_type)
         except AttributeError as e:
-            _errors.print_error(f"AttributeError: {e}; Source: {requirement.source}; requirement key: {requirement.key}.")
+            _errors.print_error(
+                f"AttributeError: {e}; Source: {requirement.source}; requirement key: {requirement.key}."
+            )
         if _errors:
             raise PolarionExportMappingError("\n".join(_errors))
         return product_types
@@ -165,7 +176,9 @@ class PolarionExporter:
         document_info["title"] = self.ig_config.title
         document_info["link"] = self.ig_config.link
         document_info["version"] = self.ig_config.version
-        document_info["date"] = f"{convert_polarion_date_export(value=self.ig_config.date)}"
+        document_info["date"] = (
+            f"{convert_polarion_date_export(value=self.ig_config.date)}"
+        )
 
         # Reserved for future configurability; currently hard-coded.
         document_info["status"] = "released"
@@ -184,9 +197,14 @@ class PolarionExporter:
                 continue
 
             if req.conformance not in self.ALLOWED_CONFORMANCE:
-                _errors.append(str(PolarionExportConformanceError(f"Conformance {req.conformance} not allowed; {req.source}; requirement key: {req.key}.")))
+                _errors.append(
+                    str(
+                        PolarionExportConformanceError(
+                            f"Conformance {req.conformance} not allowed; {req.source}; requirement key: {req.key}."
+                        )
+                    )
+                )
                 continue
-            
 
             req_export = {}
 
@@ -197,10 +215,12 @@ class PolarionExporter:
             req_export["text"] = req.text
             req_export["conformance"] = req.conformance
             req_export["characteristics"] = product_types
-            req_export["link"] = utils.convert_to_ig_requirement_link(base=self.ig_config.link,
-                                                                      source=req.source,
-                                                                      key=req.key,
-                                                                      version=req.version)
+            req_export["link"] = utils.convert_to_ig_requirement_link(
+                base=self.ig_config.link,
+                source=req.source,
+                key=req.key,
+                version=req.version,
+            )
             requirements.append(req_export)
         if _errors:
             error_msg = "\n" + "\n".join(_errors)
@@ -208,13 +228,11 @@ class PolarionExporter:
 
         data = {}
         data["document_info"] = document_info
-        data["requirements"] = requirements
+        data["requirements"] = sorted(requirements, key=lambda x: x["key"])
         self.save_export(output=output, data=data)
 
     def save_export(self, output, data):
-        ext_map = {
-            '.json': 'JSON'
-        }
+        ext_map = {".json": "JSON"}
         filepath = self.generate_filepath(output=output, version=self.version)
         base, ext = os.path.splitext(filepath)
         if ext.lower() not in ext_map:
@@ -222,16 +240,16 @@ class PolarionExporter:
 
         file_format = ext_map[ext.lower()]
 
-        dir_path = os.path.dirname(filepath) or '.'
+        dir_path = os.path.dirname(filepath) or "."
         if not os.path.exists(dir_path):
             raise FilePathNotExists(f"Path {dir_path} does not exist.")
 
-        if file_format == 'JSON':
-            with open(filepath, 'w', encoding='utf-8') as file:
+        if file_format == "JSON":
+            with open(filepath, "w", encoding="utf-8") as file:
                 json.dump(data, file, indent=4, ensure_ascii=False)
         else:
             raise ExportFormatUnknown(f"The format {file_format} is not supported.")
-        
+
 
 class PolarionCliView:
 
@@ -239,49 +257,55 @@ class PolarionCliView:
     def product_type_mapping(cls):
         ACTOR_MAPPING, TESTPROC_MAPPING = load_polarion_mappings()
         headers = [
-            ("Actor (Key)", {"colspan": 1}), 
-            ("ProductType Name", {"colspan": 1}), 
-            ("ProductType ID", {"colspan": 1}), 
-            ("ProductType desc", {"colspan": 1})
+            ("Actor (Key)", {"colspan": 1}),
+            ("ProductType Name", {"colspan": 1}),
+            ("ProductType ID", {"colspan": 1}),
+            ("ProductType desc", {"colspan": 1}),
         ]
         rows = []
         for key, value in ACTOR_MAPPING.items():
-            rows.append([
-                (f"{key}", {"colspan": 1}),
-                (f"{value.get('name')}", {"colspan": 1}),
-                (f"{value.get('id')}", {"colspan": 1}),
-                (f"{value.get('description')}", {"colspan": 1})
-            ])
+            rows.append(
+                [
+                    (f"{key}", {"colspan": 1}),
+                    (f"{value.get('name')}", {"colspan": 1}),
+                    (f"{value.get('id')}", {"colspan": 1}),
+                    (f"{value.get('description')}", {"colspan": 1}),
+                ]
+            )
         print(cli.format_table_with_border(headers=headers, rows=rows, min_width=25))
 
     @classmethod
     def test_proc_mapping(cls):
         ACTOR_MAPPING, TESTPROC_MAPPING = load_polarion_mappings()
         headers = [
-            ("Test Procedure (Key)", {"colspan": 1}), 
+            ("Test Procedure (Key)", {"colspan": 1}),
             ("Test Procedure ID", {"colspan": 1}),
-            ("Test Procedure Name", {"colspan": 1})
+            ("Test Procedure Name", {"colspan": 1}),
         ]
         rows = []
         for key, value in TESTPROC_MAPPING.items():
-            rows.append([
-                (f"{key}", {"colspan": 1}),
-                (f"{value.get('id')}", {"colspan": 1}),
-                (f"{value.get('name')}", {"colspan": 1})
-            ])
+            rows.append(
+                [
+                    (f"{key}", {"colspan": 1}),
+                    (f"{value.get('id')}", {"colspan": 1}),
+                    (f"{value.get('name')}", {"colspan": 1}),
+                ]
+            )
         print(cli.format_table_with_border(headers=headers, rows=rows, min_width=25))
 
     @classmethod
     def test_proc_default_mapping(cls):
         DEFAULT_TEST_PROCEDURE_MAPPING = load_test_procesure_default_mappings()
         headers = [
-            ("Actor (Key)", {"colspan": 1}), 
-            ("Test Procedure ID", {"colspan": 1})
+            ("Actor (Key)", {"colspan": 1}),
+            ("Test Procedure ID", {"colspan": 1}),
         ]
         rows = []
         for key, value in DEFAULT_TEST_PROCEDURE_MAPPING.items():
-            rows.append([
-                (f"{key}", {"colspan": 1}),
-                (f"{value}", {"colspan": 1}),
-            ])
+            rows.append(
+                [
+                    (f"{key}", {"colspan": 1}),
+                    (f"{value}", {"colspan": 1}),
+                ]
+            )
         print(cli.format_table_with_border(headers=headers, rows=rows, min_width=25))
